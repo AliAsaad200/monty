0c506280bdecf48ac7e20015b97e8b0f5214e8e1
AliAsaad200 <aliahmedassad2003@gmail.com>
2023-12-13 16:32:22 +0200

    w

--- pall.c
+++ pall.c
@@ -1,21 +1,64 @@
-#include "monty.h"
-/**
- * f_pall - prints the stack
- * @head: stack head
- * @counter: no used
- * Return: no return
-*/
-void f_pall(stack_t **head, unsigned int counter)
-{
-	stack_t *h;
-	(void)counter;
+#include <stdio.h>
+#include <stdlib.h>
 
-	h = *head;
-	if (h == NULL)
-		return;
-	while (h)
-	{
-		printf("%d\n", h->n);
-		h = h->next;
-	}
+#define STACK_SIZE 1000
+
+typedef struct {
+    int stack[STACK_SIZE];
+    int top;
+} Stack;
+
+Stack stack;
+
+void push(int value, int line_number) {
+    if (stack.top >= STACK_SIZE) {
+        fprintf(stderr, "L%d: Stack overflow\n", line_number);
+        exit(EXIT_FAILURE);
+    }
+    stack.stack[++stack.top] = value;
+}
+
+void pall(int line_number) {
+    int i;
+    for (i = stack.top; i > 0; i--) {
+        printf("%d\n", stack.stack[i]);
+    }
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    FILE *file = fopen(argv[1], "r");
+    if (file == NULL) {
+        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+        exit(EXIT_FAILURE);
+    }
+
+    stack.top = 0; // Initialize stack
+
+    char opcode[100];
+    int value, line_number = 0;
+
+    while (fscanf(file, "%s", opcode) == 1) {
+        line_number++;
+
+        if (strcmp(opcode, "push") == 0) {
+            if (fscanf(file, "%d", &value) != 1) {
+                fprintf(stderr, "L%d: usage: push integer\n", line_number);
+                exit(EXIT_FAILURE);
+            }
+            push(value, line_number);
+        } else if (strcmp(opcode, "pall") == 0) {
+            pall(line_number);
+        } else {
+            fprintf(stderr, "L%d: Unknown opcode: %s\n", line_number, opcode);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    fclose(file);
+    return 0;
 }
